ef470d48e38f0bfc4f33bf88f0215fc7ff4f3841
JadaBroome <49737623+JadaBroome@users.noreply.github.com>
2019-07-14 11:13:21 -0700

    restructured github

---
+++ JavaScript-I/assignments/function-conversion.js
@@ -0,0 +1,30 @@
+// Take the commented ES5 syntax and convert it to ES6 arrow Syntax
+
+let myFunction = () => {
+console.log("Function was invoked!");
+};
+myFunction();
+
+let anotherFunction = (param) => {
+  return param;
+};
+console.log(anotherFunction("Example"));
+
+let add = (param1, param2) => {
+  return param1 + param2;
+};
+console.log(add(1,2));
+
+let subtract = (param1, param2) => {
+  return param1 - param2;
+};
+console.log(subtract(1,2));
+
+
+// Stretch
+
+exampleArray = [1,2,3,4];
+const triple = exampleArray.map((num) => {
+  return num * 3;
+});
+console.log(triple);
---
+++ Sprint-Challenge--JavaScript/challenges/functions.js
@@ -0,0 +1,55 @@
+// ==== Callbacks ====
+
+/* Step 1: Create a higher-order function that accepts a callback
+ * Create a higher-order function named consume that can take 3 parameters.
+ * The first two parameters can accept any argument
+ * The last parameter accepts a callback
+ * In the body of the function return the callback with the two parameters that you created
+ */
+
+function consume(param1, param2, callback) {
+  return callback(param1, param2);
+}
+
+/* Step 2: Create several functions to callback with consume();
+ * Create a function named add that returns the sum of two numbers
+ * Create a function named multiply that returns the product of two numbers
+ * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+ */
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function greeting(fn, ln) {
+  return `Hello ${fn} ${ln}, nice to meet you!`;
+}
+/* Step 3: Check your work by un-commenting the following calls to consume(): */
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+
+// ==== Closures ====
+
+// Explain in your own words why `nestedfunction()` can access the variable `internal`.
+
+//The nested function can access the variable "internal" because it is going to look inside itself first then when it can not find it will look outside of it self until it finds what its looking for.
+
+// Explanation:
+
+const external = "I'm outside the function";
+
+function myFunction() {
+  console.log(external);
+  const internal = "Hello! I'm inside myFunction!";
+
+  function nestedFunction() {
+    console.log(internal);
+  }
+  nestedFunction();
+}
+myFunction();
